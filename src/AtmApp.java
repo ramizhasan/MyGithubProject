5a7eafdc4ca40036992966b13e2bccf7f64af0e7
Ramiz Hasan <ramizhasan2929@gmail.com>
2023-02-23 14:05:04 +0300

    V2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,6 +1,8 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
+        //versiyon2
+        System.out.println("Java is easy");
     }
 
 }
